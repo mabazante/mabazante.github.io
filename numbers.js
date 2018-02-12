7da34b1bd00e70172238cfde44f6d1028a0d0632
Maria Alejandra Bazante <mariaalejandrabazante@Marias-MacBook-Pro.local>
2018-02-12 11:33:09 -0500

    work in class

---
+++ numbers.js
@@ -0,0 +1,22 @@
+var squared = function (x) {
+  var squaredNumber = x * x;
+  return squaredNumber;
+};
+
+var plus10 = function(x) {
+  var newNumber = x+ 10;
+  return newNumber;
+};
+
+var numbers = {
+  "squared": squared,
+  "plus10": plus10
+};
+
+
+//for the html (as reference in class)
+    //var y = numbers.squared(22);
+    //console.log(y);
+
+    //var x = numbers.plus10(2);
+    //console.log(x);
